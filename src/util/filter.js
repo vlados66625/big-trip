1d03ee5ea9d877d5b57a1ebc20b0162d854895f5
Владислав <vlados66625@mail.ru>
2024-11-15 13:28:26 +0300

    исправление замеаний, добавляет копирование массива

--- src/util/filter.js
+++ src/util/filter.js
@@ -4,10 +4,10 @@ import isBetween from 'dayjs/plugin/isBetween';
 dayjs.extend(isBetween);
 
 const filter = {
-  [FilterType.EVERTHING]: (events) => events,
-  [FilterType.FUTURE]: (events) => events.filter((event) => dayjs().isBefore(dayjs(event.dateFrom))),
-  [FilterType.PRESENT]: (events) => events.filter((event) => dayjs().isBetween(event.dateFrom, event.dateTo, null, '[)')),
-  [FilterType.PAST]: (events) => events.filter((event) => dayjs().isAfter(dayjs(event.dateTo))),
+  [FilterType.EVERTHING]: (events) => [...events],
+  [FilterType.FUTURE]: (events) => [...events].filter((event) => dayjs().isBefore(dayjs(event.dateFrom))),
+  [FilterType.PRESENT]: (events) => [...events].filter((event) => dayjs().isBetween(event.dateFrom, event.dateTo, null, '[)')),
+  [FilterType.PAST]: (events) => [...events].filter((event) => dayjs().isAfter(dayjs(event.dateTo))),
 };
 
 const generateFilter = (event) =>
